4e4667375860caafcd9b74ca73af7e43829bcca5
hayunjong83 <hayunjong83@gmail.com>
2024-10-14 18:09:15 +0900

    speech mode in chatbot

--- helper.py
+++ helper.py
@@ -170,3 +170,23 @@ def diagnosis_draft_2(msg):
         messages=messages
     )
     return response.choices[0].message.content
+
+def speech_to_text(audio_data):
+    with open(audio_data, "rb") as audio_file:
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            response_format="text",
+            file=audio_file
+        )
+    return transcript
+
+def text_to_speech(input_text, webm_file_path):
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="nova",
+        input=input_text
+    )
+    # webm_file_path = "temp_audio_play.mp3"
+    with open(webm_file_path, "wb") as f:
+        response.write_to_file(webm_file_path)
+    return webm_file_path
--- page4.py
+++ page4.py
@@ -1,16 +1,35 @@
 import streamlit as st
 from openai import OpenAI
-from helper import client
+from helper import client, text_to_speech, speech_to_text
 from langsmith import traceable
+from audio_recorder_streamlit import audio_recorder
+import random
+import os
 
 @traceable
 def page_w_chatgpt():
     st.title("❔ Q&A 챗봇 [챗GPT]")
 
     with st.container(border=True):
-        st.write("- :blue[**OcuGUIDE**]에서 궁금했던 내용을 무엇이든 물어보세요.")
-        st.write("- 이해가 어려웠거나, 다시 듣고 싶으신 내용을 챗GPT 에이전트가 성심껏 답변드립니다.")
-        st.write("- 질문이 끝나신 후, 이제 곧 만나실 주치의에게 추가 질문과 설명을 들을 수 있습니다.")
+        c1, c2 = st.columns([7, 1])
+        with c1:
+            st.write("""
+                    - :blue[**OcuGUIDE**]에서 궁금했던 내용을 무엇이든 물어보세요.
+                    - ℹ️ 백내장수술정보에서 어려웠던 내용을 챗봇이 성심껏 답변드립니다.
+                    - 오른쪽의 마이크를 누르면, 음성으로 질문할 수 있습니다.
+                    - 질문이 끝나신 후, 이제 곧 만나실 주치의에게 추가 설명을 들을 수 있습니다.
+                    """)
+        with c2:
+            audio_bytes = audio_recorder(text="음성모드", neutral_color="#6aa36f", icon_size="3x")
+        # st.write("- :blue[**OcuGUIDE**]에서 궁금했던 내용을 무엇이든 물어보세요.  \n")
+        # st.write("- 이해가 어려웠거나, 다시 듣고 싶으신 내용을 챗GPT 에이전트가 성심껏 답변드립니다.  \n")
+        # st.write("- 질문이 끝나신 후, 이제 곧 만나실 주치의에게 추가 질문과 설명을 들을 수 있습니다.")
+    
+    # with st.container(border=True):
+    #     speak, listen = st.columns([1, 1])
+    #     with speak:
+    #         st.write("### 음성 도움 안내 ###")
+    #     audio_bytes = audio_recorder()
 
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-4o-mini"
@@ -38,6 +57,49 @@ def page_w_chatgpt():
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+    # st.markdown("""
+    # <style>
+    # .fixed-container {
+    #     position: fixed;
+    #     bottom: -100px;  /* chat_input 바로 위에 고정될 위치 설정 */
+    #     left: 0;
+    #     width: 100%;
+    #     background-color: white;
+    #     padding: 10px;
+    #     z-index: 9999;
+    #     text-align: center;
+    # }   
+    # </style>
+    # """, unsafe_allow_html=True)
+    
+    # st.markdown('<div class="fixed-container">', unsafe_allow_html=True)
+    # with st.container(border=True):
+    #     explain, speak, listen = st.columns([4,1,1])
+    #     with explain:
+    #         st.write("음성 도움 안내입니다.")
+    #         st.write("마이크를 눌러서 질문 후, 다시 눌러서 질문을 종료합니다.")
+    #     with speak:
+    #         audio_bytes = audio_recorder("말하기", key="before")
+    #     with listen:
+    #         on_before = st.toggle("듣기", key="toggle_before")
+    # st.markdown('</div>', unsafe_allow_html=True)
+    
+    if audio_bytes:
+        with st.spinner("Transcribing..."):
+            rand_idx = random.randint(10000, 99999)
+            webm_file_path = f"temp_{str(rand_idx)}.mp3"
+            with open(webm_file_path, "wb") as f:
+                f.write(audio_bytes)
+            
+            transcript = speech_to_text(webm_file_path)
+            st.session_state.messages.append({"role": "user", "content": transcript})
+
+            if transcript:
+                with st.chat_message("user"):
+                    st.markdown(transcript)
+                os.remove(webm_file_path)
+
+            
     if prompt := st.chat_input("궁금한 사항을 물어봐주세요."):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
@@ -54,4 +116,22 @@ def page_w_chatgpt():
             )
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
+        # #     # 고정된 위치에 container
+        # # st.markdown('<div class="fixed-container">', unsafe_allow_html=True)
+        # # with st.container(border=True):
+        # #     st.write("This is a container fixed above the chat input.")
+        # #     st.write("This is a container fixed above the chat input.")
+        # # st.markdown('</div>', unsafe_allow_html=True)    
+        # st.markdown('<div class="fixed-container">', unsafe_allow_html=True)
+        # with st.container(border=True):
+        #     explain, speak, listen = st.columns([4,1,1])
+        #     with explain:
+        #         st.write("음성 도움 안내입니다.")
+        #         st.write("마이크를 눌러서 질문 후, 다시 눌러서 질문을 종료합니다.")
+        #     with speak:
+        #         audio_bytes = audio_recorder("말하기", key="after")
+        #     with listen:
+        #         on_after = st.toggle("듣기", key="toggle_after")
+        # st.markdown('</div>', unsafe_allow_html=True)
 
+        
